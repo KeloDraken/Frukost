a09089230fe8241beb8feb2cda7af87915a40a7d
Samkelo <joymama078@gmail.com>
2021-11-26 21:07:45 +0200

    Create User model

--- vetkoek/config/settings.py
+++ vetkoek/config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # Our apps
-    "accounts",
+    "core",
+    "core.accounts",
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = "accounts.User"
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
--- vetkoek/core/accounts/models.py
+++ vetkoek/core/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Users within the ChafPozi authentication system are represented by this model.
+
+    Username and password are required. Other fields are optional.
+    """
+    pass
+
