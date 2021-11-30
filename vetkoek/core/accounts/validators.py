5d0101cd8894781e1ceb962b2e9629781bcbb412
Samkelo <joymama078@gmail.com>
2021-11-30 20:04:45 +0200

    Add docstrings & clean up

--- vetkoek/core/accounts/validators.py
+++ vetkoek/core/accounts/validators.py
@@ -1,5 +1,3 @@
-import re
-
 from django.core import validators
 from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
@@ -7,7 +5,10 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class UnicodeEmailValidator(validators.RegexValidator):
+    """
+    Regex that validates that email is a WeThinkCode_ email, student or staff.
+    """
+
     regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.wethinkcode.co.za+$)"
-    # regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
     message = _("Enter a valid WeThinkCode email address.")
     flags = 0
