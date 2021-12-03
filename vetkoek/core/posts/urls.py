4e5322237b2bb585cc468ecd700db05be0af8651
Samkelo <joymama078@gmail.com>
2021-12-03 15:07:18 +0200

    Remove photo from view post url pattern

--- vetkoek/core/posts/urls.py
+++ vetkoek/core/posts/urls.py
@@ -7,7 +7,7 @@ app_name = "posts"
 
 urlpatterns = [
     path("", frontpage, name="frontpage"),
-    path("photo/<post_id>/", get_post, name="get-post"),
     path("add/", create_post, name="create-post"),
     path("delete/<post_id>", delete_post, name="delete-post"),
+    path("<post_id>/", get_post, name="get-post"),
 ]
