90edfd4a42592bbe822ed58a6208e29ba8753f16
Samkelo <joymama078@gmail.com>
2021-12-04 14:22:59 +0200

    Create a mesonry layout in frontpage

--- vetkoek/core/accounts/forms.py
+++ vetkoek/core/accounts/forms.py
@@ -156,64 +156,6 @@ class EditUserProfileForm(UserChangeForm):
         ),
     )
 
-    instagram = forms.URLField(
-        required=False,
-        max_length=60,
-        label="",
-        widget=forms.TextInput(
-            attrs={
-                "class": "w-full text-black px-3 py-2 mb-3 border-2 border-gray-300 rounded bg-white",
-                "placeholder": "Instagram profile url",
-                "autocomplete": "off",
-                "name": "instagram",
-                "autocapitalize": "off",
-            }
-        ),
-    )
-    
-    vsco = forms.URLField(
-        required=False,
-        max_length=60,
-        label="",
-        widget=forms.TextInput(
-            attrs={
-                "class": "w-full text-black px-3 py-2 mb-3 border-2 border-gray-300 rounded bg-white",
-                "placeholder": "VSCO profile url",
-                "autocomplete": "off",
-                "name": "vsco",
-                "autocapitalize": "off",
-            }
-        ),
-    )
-    
-    twitter = forms.URLField(
-        required=False,
-        max_length=60,
-        label="",
-        widget=forms.TextInput(
-            attrs={
-                "class": "w-full text-black px-3 py-2 mb-3 border-2 border-gray-300 rounded bg-white",
-                "placeholder": "Twitter profile url",
-                "autocomplete": "off",
-                "name": "twitter",
-                "autocapitalize": "off",
-            }
-        ),
-    )
-    
-    website = forms.URLField(
-        required=False,
-        max_length=60,
-        widget=forms.TextInput(
-            attrs={
-                "class": "w-full text-black px-3 py-2 mb-3 border-2 border-gray-300 rounded bg-white",
-                "placeholder": "Add a website",
-                "autocomplete": "off",
-                "name": "website",
-                "autocapitalize": "off",
-            }
-        ),
-    )
     class Meta:
         model = User
         fields = (
@@ -221,8 +163,4 @@ class EditUserProfileForm(UserChangeForm):
             "display_name",
             "bio",
             "custom_html",
-            "instagram",
-            "vsco",
-            "twitter",
-            "website",
         )
