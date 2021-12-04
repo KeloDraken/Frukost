ecb521c29899a64eead5359abd707ad530921c3b
Samkelo <joymama078@gmail.com>
2021-12-04 20:51:29 +0200

    Create communities app

--- vetkoek/core/search/urls.py
+++ vetkoek/core/search/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from core.search.views import search
 
 
-app_name = 'search'
-urlpatterns = [
-    path('', search, name='search')
-]
+app_name = "search"
+
+urlpatterns = [path("", search, name="search")]
