8284087970ad44ae6d66e3aa9a938170d6972f04
Samkelo <joymama078@gmail.com>
2021-12-06 17:42:32 +0200

    Add prod settings

--- vetkoek/config/settings.py
+++ vetkoek/config/settings.py
@@ -92,8 +92,12 @@ if DEBUG:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "msukwini",
+            "USER": "samkelo",
+            "PASSWORD": "Jm218071748*",
+            "HOST": "localhost",
+            "PORT": "",
         }
     }
 
